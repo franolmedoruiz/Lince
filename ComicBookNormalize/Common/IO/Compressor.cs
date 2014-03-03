8ff25d937a037933b409afd450885a16fa1c746f
franolmedoruiz <franolmedoruiz@gmail.com>
2014-03-03 19:01:53 +0100

    Adding Common and Core projects.

---
+++ ComicBookNormalize/Common/IO/Compressor.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using SharpCompress.Common;
+using SharpCompress.Reader;
+using SharpCompress.Writer;
+
+namespace ComicBookNormalize.Common.IO
+{
+    public class Compressor
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="output"></param>
+        public static void Decompress(string file, string output)
+        {
+            using (Stream stream = File.OpenRead(file))
+            {
+                var reader = ReaderFactory.Open(stream);
+                while (reader.MoveToNextEntry())
+                {
+                    if (!reader.Entry.IsDirectory)
+                        reader.WriteEntryToDirectory(output, ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="file"></param>
+        public static void Compress(string source, string file)
+        {
+            using (Stream stream = File.OpenWrite(file))
+            {
+                using (var writer = WriterFactory.Open(stream, ArchiveType.Zip, CompressionType.LZMA))
+                {
+                    writer.WriteAll(source, "*", SearchOption.AllDirectories);
+                }
+            }
+        }
+    }
+}
---
+++ ComicBookNormalize/Core/Package/cbrPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookNormalize.Core.Package
+{
+    public class cbrPackage
+    {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
